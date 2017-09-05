ec795bf61f8ba0d099605b54234660e7c4594ebb
Sebastian Nilsson <sebastian@typeof.nu>
2017-09-06 00:37:46 +0200

    Training model is working! :)

---
+++ gpu-test.py
@@ -0,0 +1,14 @@
+
+
+# Check if we have Tensorflow GPU
+# tf.__version__sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+from tensorflow.python.client import device_lib
+
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+
+print(device_lib.list_local_devices())
+
